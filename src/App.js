af71a4e67611abb194a4086d2e1a5b79e742ca35
Caleb Schmidt <cdschmidt1103@gmail.com>
2022-10-26 19:57:33 -0400

    added page views, themes, and context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import AdminMenu from './components/AdminMenu';
+import NavBar from './components/NavBar';
+import {Route, Routes} from 'react-router-dom';
+import SnackBar from './components/SnackBar';
+import Box from '@mui/material/Box';
+import {AppContext} from './context/AppContext';
+import {useContext} from 'react';
+import Login from './views/Login';
+
+
+
+const HomePage = () => (<h1>Welcome To The Bookstore!</h1>)
 
 function App() {
+  const {user} = useContext(AppContext)
+
+    // <Route path = "/register" element = {<Register />} />
+
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <SnackBar/>
+      <NavBar>
+        <Box sx = {{minHeight:'90vh'}}>
+          <Routes>
+            <Route path = "/" element = {<HomePage />} />
+            <Route path = "/login" element = {<Login />} />
+          </Routes>
+        </Box>
+      {user?.is_admin?<AdminMenu/>:<></>}
+      </NavBar>
+    </>
   );
 }
 
